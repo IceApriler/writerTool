562e190405f2bcde35dce49fd7520884b741edb4
april <qimengjie@163.com>
2018-08-22 13:28:02 +0800

    feat: init tree resolver

---
+++ src/renderer/components/resolver.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="resolver"
+       ref="resolver">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'resolver',
+  props: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
+<style lang="less">
+.resolver {
+}
+</style>
